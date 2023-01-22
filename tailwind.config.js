03f7a5c2b312b4c20137da45c949707b08e04f2e
John Phillips <john@devmanjohn.com>
2023-01-22 14:00:39 +0000

    typography settings added to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-​
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // defaults
+        'dark-blue': '#070724',
+        'dark-gray': '#38384F',
+        'light-gray': '#838391',
+
+        // planet colors
+        mercury: '#419EBB',
+        venus: '#EDA249',
+        earth: '#6f2ed6',
+        mars: '#419EBB',
+        jupiter: '#D83A34',
+        saturn: '#CD5120',
+        uranus: '#1ec2a4',
+        neptune: '#2d68f0',
+      },
+
+      fontFamily: {
+        spartan: ['League Spartan', sans - serif],
+        antonio: ['League Spartan', sans - serif],
+      },
+    },
   },
   plugins: [],
-}
+};
