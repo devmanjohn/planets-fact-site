755dd5ed0e1a925e0668be9b1cde19f864b96236
John Phillips <john@devmanjohn.com>
2023-01-24 21:51:02 +0000

    fixed issue with svg

--- components/header/Navbar.tsx
+++ components/header/Navbar.tsx
@@ -32,7 +32,7 @@ export default function Navbar() {
           }}
         />
         <svg xmlns='http://www.w3.org/2000/svg' width='24' height='17'>
-          <g fill='#FFF' fill-rule='evenodd'>
+          <g fill='#FFF' fillRule='evenodd'>
             <path d='M0 0h24v3H0zM0 7h24v3H0zM0 14h24v3H0z' />
           </g>
         </svg>
